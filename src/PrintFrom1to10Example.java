afc1cd80a58c2f84666b8693b9b0d27a76a0efde
jeong <deas124@gmail.com>
2023-01-06 18:49:00 +0900

    18:48

---
+++ src/PrintFrom1to10Example.java
@@ -0,0 +1,12 @@
+
+public class PrintFrom1to10Example {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		System.out.println("프로그램 종료");
+	}
+
+}
